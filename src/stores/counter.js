8cae40302f1f103ecae5c28a88efc0cf3458d296
gc <420045389@qq.com>
2023-11-29 16:36:32 +0800

    ✨ feat:提交测试

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -6,6 +6,7 @@ export const useCounterStore = defineStore('counter', () => {
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
+    console.log(count.value);
   }
 
   return { count, doubleCount, increment }
