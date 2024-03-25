7b224d8a43cad550830d411c5d194a90957a17f2
420045389 <420045389@qq.com>
2024-03-25 16:58:42 +0800

    ✨ feat:添加接口实例

--- src/https/api/upload.js
+++ src/https/api/upload.js
@@ -3,6 +3,6 @@
  * @params image_file[] 文件file
  * @params type  上传类型
  */
-export function uploadMedia(data, config = { headers: { 'Content-Type': 'multipart/form-data' } }) {
+export function userUploadMedia(data, config = { headers: { 'Content-Type': 'multipart/form-data' } }) {
   return request.post('/user/uploadmedia', data, config)
 }
