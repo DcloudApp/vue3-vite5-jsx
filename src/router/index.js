5af3c67094b04227a2e6e9ef9121d333a5392edc
420045389 <420045389@qq.com>
2024-03-28 15:34:24 +0800

    ✨ feat:增加版本控制，更新清除本地存储

--- src/router/index.js
+++ src/router/index.js
@@ -23,5 +23,12 @@ const router = createRouter({
     },
   ],
 })
+router.beforeEach(async (to, from, next) => {
+  if (localStorage.getItem('appVersion') !== import.meta.env.VITE_APP_VERSION) {
+    localStorage.clear() // 清除所有本地存储
+    localStorage.setItem('appVersion', import.meta.env.VITE_APP_VERSION) // 更新版本号
+  }
+  next()
+})
 
 export default router
